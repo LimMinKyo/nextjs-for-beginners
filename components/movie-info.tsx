e30dd2e39831b238dcd591dd4b70ab6e280d6f55
LimMinKyo <story1400@naver.com>
2024-05-08 09:48:33 +0900

    3.4 Dynamic Metadata

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { API_URL } from "../app/(home)/page";
 import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
@@ -19,7 +19,7 @@ export default async function MovieInfo({ id }: { id: string }) {
       />
       <div className={styles.info}>
         <h1 className={styles.title}>{movie.title}</h1>
-        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <h3>⭐️ {movie.vote_average.toFixed()}</h3>
         <p>{movie.overview}</p>
         <a href={movie.homepage} target={"_blank"}>
           Homepage &rarr;
